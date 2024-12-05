fb77da0d95425bd03c4aa74996af4efeada394cb
Igromanoff <mister.igromanoff@gmail.com>
2024-12-05 21:32:57 +0300

    Update Disable NVIDIA Display Container LS.cmd

--- Bin/Disable NVIDIA Display Container LS.cmd
+++ Bin/Disable NVIDIA Display Container LS.cmd
@@ -5,29 +5,16 @@ whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
 	exit /b
 )
 
-:: check if the service exists
 reg query "HKLM\SYSTEM\CurrentControlSet\Services\NVDisplay.ContainerLocalSystem" > nul 2>&1 || (
-    echo The NVIDIA Display Container LS service does not exist, you cannot continue.
-	echo You may not have NVIDIA drivers installed.
+    echo Служба NVIDIA Display Container LS не существует, вы не можете продолжить.
+	echo Возможно, у вас не установлены драйверы NVIDIA.
     echo]
     pause
     exit /b 1
 )
 
-echo Disabling the 'NVIDIA Display Container LS' service will stop the NVIDIA Control Panel from working.
-echo It will most likely break other NVIDIA driver features as well.
-echo These scripts are aimed at users that have a stripped driver, and people that barely touch the NVIDIA Control Panel.
-echo]
-echo You can enable the NVIDIA Control Panel and the service again by running the enable script.
-echo Additionally, you can add a context menu to the desktop with another script in the Atlas folder.
-echo]
-echo See 'Must Read First' for more info.
-echo]
-pause
-
+echo Отключение службы "NVIDIA Display Container LS" приведет к прекращению работы панели управления NVIDIA.
+echo Скорее всего, это также нарушит работу других функций драйвера NVIDIA.
+echo.
 call setSvc.cmd NVDisplay.ContainerLocalSystem 4
 sc stop NVDisplay.ContainerLocalSystem > nul 2>&1
-
-echo Finished, changes have been applied.
-pause
-exit /b
